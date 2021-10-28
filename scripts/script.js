44fc5cb12d2abff06ca279ee0c9947a4466bbe6d
Breno Froes <breno_froes@id.uff.br>
2021-10-28 16:58:40 -0300

    [feature] Ajuste animacoes de navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,59 @@
-var data = '2021/11/30';
-var falta = (new Date(data).getTime() - new Date().getTime()) / 1000;
-var segundos = Math.round(falta % 60);
-var minutos = Math.round(falta / 60 % 60);
-var horas = Math.round(falta / 60 / 60 % 24);
-var dias = Math.round(falta / 60 / 60 / 24);
-var divs = document.querySelectorAll('.time');
+// Variaveis para animacao do navbar
+const navbar = document.querySelector("#navbar");
+const button = document.querySelector("#button-blue");
+let buttonFixed = document.querySelector("#button-blue-fixed");
+const col = document.querySelector(".col-12");
+const stickyNavbar = navbar.offsetTop;
+const stickyButton = button.offsetTop;
+
+window.onscroll = () => {
+    let windowY = window.pageYOffset;
+    if (windowY == 0) {
+        navbar.classList.remove("sticky");
+    }
+    // Se a tela for grande
+    if(window.screen.width >= 991){
+        if (windowY >= stickyNavbar) {
+            navbar.classList.add("sticky");
+            if (windowY >= 4000) {
+                footer.classList.add("sticky");
+                navbar.classList.add("reposition-sticky");
+            } else {
+                footer.classList.remove("sticky");
+                navbar.classList.remove("reposition-sticky");
+            }
+        } else {
+            navbar.classList.remove("sticky");
+            navbar.classList.remove("reposition-sticky");
+        }
+    // Se a tela for pelo menos média
+    } else {
+        if (windowY >= stickyButton -555) {
+            col.classList.add("col-0");
+            buttonFixed.style.display = "flex";
+            footer.classList.remove("sticky");
+            navbar.classList.remove("reposition-sticky");
+            navbar.classList.remove("bg-black");
+    
+        } else {
+            col.classList.remove("col-0");
+            buttonFixed.style.display = "none";
+            navbar.classList.add("bg-black");
+            navbar.classList.remove("reposition-sticky");
+        }
+    }
+    
+    
+};
 
 setInterval(() => {
+    const data = '2021/11/30';
+    let falta = (new Date(data).getTime() - new Date().getTime()) / 1000;
+    let segundos = Math.round(falta % 60);
+    let minutos = Math.round(falta / 60 % 60);
+    let horas = Math.round(falta / 60 / 60 % 24);
+    let dias = Math.round(falta / 60 / 60 / 24);
+    let divs = document.querySelectorAll('.time');
     if (segundos == 0) {
         segundos = 60;
         minutos--;
@@ -20,7 +67,7 @@ setInterval(() => {
         dias--;
     }
     segundos--;
-    var contador = [dias, horas, minutos, segundos].forEach((parcela, i) =>{
+    let contador = [dias, horas, minutos, segundos].forEach((parcela, i) =>{
         switch(i){
             case 0:
                 document.querySelector(".day").innerHTML = 
@@ -46,28 +93,4 @@ setInterval(() => {
     });
 }, 1000);
 
-window.onload = () =>{
-    const navbar = document.querySelector("#navbar");
-    const button = document.querySelector("#blue-button");
-    const stickyNavbar = navbar.offsetTop;
-    const stickyButton = button.offsetTop;
-}
-
-window.onscroll = () => {
-    console.log(sticky);
-    if (window.pageYOffset >= stickyNavbar) {
-        navbar.classList.add("sticky");
-        button.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-        navbar.classList.remove("reposition-sticky");
-    }
-    if (window.pageYOffset >= 4000) {
-        footer.classList.add("sticky");
-        navbar.classList.add("reposition-sticky");
-    } else {
-        footer.classList.remove("sticky");
-        navbar.classList.remove("reposition-sticky");
-    }
-};
 
